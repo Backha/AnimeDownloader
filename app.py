2e44eb62d14123a65bcd07f91406e61509228ce5
Backha <backha7@gmail.com>
2024-11-12 21:31:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask, request, render_template
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        anime_name = request.form['anime_name']
+        return f"Вы ввели: {anime_name}"
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
