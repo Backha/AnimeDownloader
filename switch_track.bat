0c60b1f4ad5517dcb50ed66f0cc95ecfe92739ed
Backha <backha7@gmail.com>
2024-11-19 23:28:39 +0300

    Update switch_track.bat

--- switch_track.bat
+++ switch_track.bat
@@ -12,34 +12,37 @@ set "log_file=F:\Anime\process_log.txt"
 :: Очищаем/создаём лог-файл
 echo Script started on %date% at %time% > "%log_file%"
 
-:: Рекурсивный поиск файлов в папках, содержащих "AniLibria"
+:: Рекурсивный поиск папок с названием "AniLibria"
 for /d %%D in ("%base_folder%\*AniLibria*") do (
-    echo Checking folder: %%D >> "%log_file%"
-    
+    echo Analyzing folder: %%D
+    echo Analyzing folder: %%D >> "%log_file%"
+
     set "audio_count=0"
     set "subtitle_count=0"
     set "skip_folder=no"
 
-    :: Проверяем все файлы в папке
+    :: Проверка файлов в папке
     for %%F in ("%%D\*.mkv") do (
-        echo Analyzing file: %%F >> "%log_file%"
-        
+        echo Found file: %%F
+        echo Found file: %%F >> "%log_file%"
+
         "%mkvinfo_path%" "%%F" | findstr /C:"Track type: audio" /C:"Track type: subtitles" > temp_check.txt
 
         :: Подсчитываем количество треков
         for /f "tokens=*" %%L in (temp_check.txt) do (
+            echo Checking line: %%L >> "%log_file%"
             if /i "%%L"=="|  + Track type: audio" set /a audio_count+=1
             if /i "%%L"=="|  + Track type: subtitles" set /a subtitle_count+=1
         )
 
-        :: Проверка условий
+        :: Проверяем на минимум 2 трека
         if !audio_count! lss 2 (
-            echo Not enough audio tracks in folder %%D. Skipping... >> "%log_file%"
+            echo Not enough audio tracks in %%F. Skipping folder %%D. >> "%log_file%"
             set "skip_folder=yes"
             goto :skip_folder
         )
         if !subtitle_count! lss 2 (
-            echo Not enough subtitle tracks in folder %%D. Skipping... >> "%log_file%"
+            echo Not enough subtitle tracks in %%F. Skipping folder %%D. >> "%log_file%"
             set "skip_folder=yes"
             goto :skip_folder
         )
@@ -47,13 +50,14 @@ for /d %%D in ("%base_folder%\*AniLibria*") do (
 
     :skip_folder
     if "!skip_folder!"=="yes" (
-        echo Folder skipped: %%D >> "%log_file%"
+        echo Skipping folder: %%D due to insufficient tracks. >> "%log_file%"
         del temp_check.txt
         goto :next_folder
     )
 
-    :: Обрабатываем файлы в папке, если все проверки пройдены
+    :: Обрабатываем файлы
     for %%F in ("%%D\*.mkv") do (
+        echo Processing file: %%F
         echo Processing file: %%F >> "%log_file%"
         "%mkvpropedit_path%" "%%F" --edit track:2 --set flag-default=1 --set flag-forced=0 --edit track:3 --set flag-default=0 --set flag-forced=1 --edit track:4 --set flag-default=1 --set flag-forced=0 --edit track:5 --set flag-default=0 --set flag-forced=1 >> "%log_file%" 2>&1
 
