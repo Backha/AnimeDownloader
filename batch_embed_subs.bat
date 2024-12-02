ffd0686078f1a1daf92ae0e772a8cf4fd94f8e32
Backha <backha7@gmail.com>
2024-12-03 01:13:49 +0300

    Update batch_embed_subs.bat

--- batch_embed_subs.bat
+++ batch_embed_subs.bat
@@ -1,62 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Ask the user for subtitle language
-set /p subtitle_lang=Enter subtitle language (en/ru): 
+:: Output the script's directory
+echo Script is running from: %~dp0
 
-:: Validate user input
-if not "!subtitle_lang!"=="en" if not "!subtitle_lang!"=="ru" (
-    echo Invalid input. Use "en" or "ru".
-    pause
-    exit /b
-)
-
-:: Set track name based on language
-if "!subtitle_lang!"=="en" (
-    set "track_name=English"
-) else (
-    set "track_name=Русский"
-)
-
-:: Specify the path to MKVToolNix
-set "mkvmerge_path=C:\Program Files\MKVToolNix\mkvmerge.exe"
-
-:: Create a log file
-set "log_file=process_log.txt"
-echo Script started on %date% at %time% > "%log_file%"
-
-:: Change to the folder with video files
+:: Switch to the script's directory
 cd /d "%~dp0"
-echo Current folder: %cd% >> "%log_file%"
+
+:: Output the current directory after switching
+echo Current working directory: %cd%
 
 :: Check for MKV files
 for %%A in (*.mkv) do (
-    echo Found video file: %%A >> "%log_file%"
-    echo Found video file: %%A
-
-    :: Check for matching subtitle file
-    set "subtitle_file="
-    for %%S in ("%%~nA*.ass") do (
-        set "subtitle_file=%%S"
-        goto ProcessFile
-    )
-
-    echo No subtitles found for: %%A >> "%log_file%"
-    echo No subtitles found for: %%A
-    goto SkipFile
-
-:ProcessFile
-    echo Processing file: %%A with subtitles: !subtitle_file! >> "%log_file%"
-    echo Processing file: %%A with subtitles: !subtitle_file!
-
-    :: Dummy operation for debugging
-    echo mkvmerge would run here on %%A and !subtitle_file! >> "%log_file%"
-
-:SkipFile
+    echo Found file: %%A
 )
 
-echo Script finished on %date% at %time% >> "%log_file%"
-echo Script finished on %date% at %time%
+:: Check if no MKV files were found
+if not exist *.mkv (
+    echo No MKV files found in this directory.
+)
 
-endlocal
 pause
